626ef9695dd4dfc576209d0a590742663caff37f
Rayane Tenório <rayanemtenorio@gmail.com>
2022-08-07 00:08:41 -0300

    theme color mobile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#3F1749">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#3F1749">
+        <meta name="msapplication-navbutton-color" content="#3F1749">
 
         <title inertia>Rayane Tenório Dev</title>
 
